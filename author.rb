8683d0c1354c356104c36007e8e4e44859f012f1
github username <github email address>
2018-12-18 09:50:48 +0000

    first commit

--- author.rb
+++ author.rb
@@ -1,1 +1,36 @@
+class Author
 
+@@all = []
+
+attr_accessor :name, :title, :word_count
+
+def initialize(name)
+@name = name
+@@all << self
+end
+
+def self.all
+  @@all
+end
+
+def books
+  Book.all.select {|book| book.author == self }
+end
+
+def write_book(title, word_count)
+  Book.new(title, self, word_count)
+end
+
+def total_words
+  running_total = 0
+  books.each do |book|
+    running_total += book.word_count
+  end
+running_total
+end
+
+def self.most_words
+  Author.all.max_by {|author| author.total_words}
+end
+
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,14 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+author1 = Author.new("Sally Rooney")
+author2 = Author.new("Ian Rankin")
+author3 = Author.new("Elif Shafak")
+
+book1 = Book.new("Honour", author3, 340)
+book2 = Book.new("Knots and Crosses", author2, 250)
+book3 = Book.new("Normal People", author1, 280)
+book4 = Book.new("Conversations with Friends", author1, 320)
+
 binding.pry
-
-
+p 'eof'
